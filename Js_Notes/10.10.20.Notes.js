2bf542ca799caee86b1c830754fa763ac5a120c1
Tauseef-Coder <lm4cg33k@aol.com>
2020-10-10 13:57:52 -0400

    todays notes updated added methods

---
+++ Js_Notes/10.10.20.Notes.js
@@ -0,0 +1,41 @@
+//let quote= "\"whats up\" "
+//console.log(quote)
+
+//let Tauseef ='Tauseef Gordon'
+//console.log(Tauseef);
+//let Gordon ='Tauseef\n Gordon'
+//console.log(Gordon)
+// let expamble1 = "nebula"
+//console.log(example1);
+//console.log(example[0]);
+//console.log(example[1]);
+
+///let myName = "Tauseef Gordon";
+//console.log(myName[0]);
+//console.log(myName[8]);
+//console.log(`${myName[0]}${myName[7]}`);//older way
+//let firstLetter = myName[0];
+//let secondLetter = myName[8];
+//console.log(firstLetter + secondLetter);
+
+//let sentence = "Tauseef Gordon"
+//console.log
+//console.log(sentence);
+////console.log(sentence.length);
+//console.log(sentence[13]);
+//console.log(sentence[sentence.length -1]);
+
+
+let welcome = "hi";
+console.log(welcome.charAt(4));
+console.log(welcome.charAt(2));
+console.log(welcome.charAt(1));
+
+//console.log(welcome);  
+//console.log(welcome.toLocaleUpperCase());
+//console.log(welcome.toLocaleLowerCase());
+console.log(welcome.replace("hi, hello"));
+let name ="Tauseef";
+console.log(name.Replace(/a/g,"-"));
+
+
