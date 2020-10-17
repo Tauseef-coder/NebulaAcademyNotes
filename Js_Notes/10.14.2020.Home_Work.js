92c53ca52750197c7b4064014187ee0d33d4ce21
Tauseef-Coder <lm4cg33k@aol.com>
2020-10-17 08:54:23 -0400

    home work assignment

---
+++ Js_Notes/10.14.2020.Home_Work.js
@@ -0,0 +1,16 @@
+let S ="sofware"
+console.log (S[S.length- 7 ]);
+let web ="web,engineer"
+console.log (web);
+console.log (web.includes("w"));
+console.log(web.includes("engineer"));
+let words = "software, academy"
+console.log (words.split(","));
+console.log (words.length);
+console.log (words[words.length - 1]);
+console.log (words [5]);
+console.log (words.replace('a','-'));
+console.log (words.toUpperCase());
+console.log (words.toLocaleLowerCase());
+let flavor ="chocolate"
+console.log = (flavor);
