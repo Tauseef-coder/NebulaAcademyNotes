8770fbb2cd480be50812bd9181b79b644c76bbba
Tauseef-Coder <lm4cg33k@aol.com>
2020-10-19 17:26:48 -0400

    Home work submitted 5:25pm

---
+++ Js_Notes/10.17.2020.Home_Work.js
@@ -0,0 +1,13 @@
+console.log(15/2);
+console.log(30/2);
+// i feel like i might have skipped somemthing in the instruction.
+let num = 5
+console.log (5 =='5');
+console.log (5 === '5');
+console.log (5!='5');
+console.log (5!=='5');
+console.log (5 == 5 && 5 ==6);
+console.log (5==5|| 5 ==6);
+///let num =3
+console.log(Math.floor(Math.random() * 10.00000));
+console.log(Math.floor(Math.random() * 1000));
