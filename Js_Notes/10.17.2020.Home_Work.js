7afea36f3d29e8564c5f7929a8f1a97a64cde3b3
Tauseef-Coder <lm4cg33k@aol.com>
2020-10-19 20:52:11 -0400

    todays notes

--- Js_Notes/10.17.2020.Home_Work.js
+++ Js_Notes/10.17.2020.Home_Work.js
@@ -1,7 +1,10 @@
 console.log(15/2);
+console.log(15%2);
+let num = 15%2
+console.log(num);
 console.log(30/2);
 // i feel like i might have skipped somemthing in the instruction.
-let num = 5
+//let num = 5
 console.log (5 =='5');
 console.log (5 === '5');
 console.log (5!='5');
@@ -9,5 +12,5 @@ console.log (5!=='5');
 console.log (5 == 5 && 5 ==6);
 console.log (5==5|| 5 ==6);
 ///let num =3
-console.log(Math.floor(Math.random() * 10.00000));
+console.log(Math.random(Math.floor() * 1000));
 console.log(Math.floor(Math.random() * 1000));
---
+++ Js_Notes/10.19.2020.Notes.js
@@ -0,0 +1,36 @@
+//console.log(15/2);
+//console.log(15%2);
+//let num = 15%2
+////.log(num);
+//console.log(30/2);
+// i feel like i might have skipped somemthing in the instruction.
+//let num = 5
+//console.log (5 =='5');
+////console.log (5 === '5');
+//console.log (5!='5');
+//console.log (5!=='5');
+//console.log (5 == 5 && 5 ==6);
+//console.log (5==5|| 5 ==6);
+///let num =3
+//console.log(Math.random(Math.floor() * 100));//math.random give you a decimal 
+//console.log(Math.floor(Math.random() * 1000));/// math. floor give you a hole number each time 
+//-----------
+console.log(Boolean(0));// false
+console.log (Boolean(10 > 9));//true
+///Language	AND	OR	NOT
+//C++	&&	||	!
+//C#	&&	||	!
+//Java	&&	||	!
+//JavaScript	&&	||	!
+//Python	and	or	not
+//Swift	&&	||	!
+//alert( true || true );   // true
+//alert( false || true );  // true
+//alert( true || false );  // true
+//alert( false || false ); // false
+//typeof is an operator 
+console.log(typeof 6);//number
+console.log(typeof 'hi');//string
+console.log(typeof{})
+
+///elemets are the values inside an array 
