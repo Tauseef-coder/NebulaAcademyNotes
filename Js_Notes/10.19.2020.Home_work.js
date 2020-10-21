95b5c8ffdc55917f840ccbe4ff14f4b306e9a223
Tauseef-Coder <lm4cg33k@aol.com>
2020-10-21 17:13:41 -0400

    home work submitted

---
+++ Js_Notes/10.19.2020.Home_work.js
@@ -0,0 +1,16 @@
+Math ="2"
+console.log (Math);
+let homeWork = 'homeWork'
+console.log (homeWork);
+NaN === NaN;
+console.log (NaN);
+//Array.isArray()
+let fruits =['apple', 'orange', 'peach', 'lemon', 'plum', 'grape', 'watermelon'] 
+console.log (fruits);
+console.log (fruits.length);
+console.log (fruits[4]);
+console.log (fruits [fruits.length- 1]);
+console.log (fruits[fruits.length - 5 ]);
+console.log (fruits.pop ());
+console.log (fruits);
+console.log (fruits.push ('banana'));/// time to turn assiggment in 
