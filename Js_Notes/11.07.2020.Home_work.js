2c1844ab4c6c768f4e1969d3c330352628c072bf
Tauseef-Coder <lm4cg33k@aol.com>
2020-11-11 17:21:38 -0500

    homw work

--- Js_Notes/11.07.2020.Home_work.js
+++ Js_Notes/11.07.2020.Home_work.js
@@ -2,51 +2,59 @@
 // Create a function that loops through any string passed into the function.
 // Log every character in this string
 // Once all characters are successfully logged, return the string, 'Done!'
-function functionLoops("Tauseef"){
-    let i;
-    for (i =0;  functionLoops.length; i++){
+function functionLoops(tauseef){
+    
+    for (let i; i = 0; <  functionLoops.length - 1 ; i++){
+        console.log(tauseef.lenght[i])
+
         return "done!"
     }
 
 }
-console.log (functionLoops);
+console.log (functionLoops('tauseef'));
 
 
 // Create a function that takes in a number as a parameter.
 // This function will log from 1- any number passed in this function as an argument
 // After all numbers are successfully logged, return the string, 'Done!'
-funnction functionNum("3"){
-    for (i =0 ; functionNum.length; i++){
+funnction functionNum(num3){
+    for (i =1; num3.length; i++){
         return "done!"
 
     }
     
 }
-console.log(functionNum);
+console.log(functionNum(num3));
 // Create a function that takes in an array as a parameter.
 // This function will Loop/iterate through the array and log every element inside the array
 // After all elements are logged, return the string, 'Done!'
 let array =['Monday','Tuesday','Wednesday','Thurday','Friday']
-function functionArray(){
-    for (i = 0; functionArray.length; i ++){
+function functionArray(stringArray){
+    for (i = 0; stringArray.length; i ++){
+        console.log(stringArray[i]);
         return "done!"
     }
     
-}
+}console.log(functionArray(array));
 // Create a function that takes in a string.
 // This function will loop through the string and log the indices of each character in the string.
 // After the indices are logged, return the string, 'Done!'
-function stringFunction('1','2','3'){
+let snowBall ='manifest destiny'
+function stringFunction(indication){
     let i;
-    for(i=0; stringFunction.log;    i++){
-
+    for(i=0; i= indication.lenght;  i++){
+        console.log(indication[i])
     }
     return "done"
 
 } console.log(stringFunction.length);
 // Create a function that only logs the even numbers from 1-10
 // After the even numbers are logged, return the string, 'Done!'
-function logFunction(){
+let num=[1,2,3,4,5,6,7,8,9,10]
+function logFunction(num){
+    for (let i=1 ,num.lenght; i+2){
+        console.log(num(i))
+    }
 
     return 'done!'
 }
---
+++ Js_Notes/11.09.2020.Notes.js
@@ -0,0 +1,32 @@
+// PRACTICE PROBLEMS 
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+functionArray addNums(numArr){
+    let total =0;
+
+for (let i = 0 i number.lenght; i++){
+    toatoal=+numArr[i]
+
+} return total 
+}
+console.log(addNums(array));
+// add all of the numbers in the array
+fucantionn only
+console.log(array(array.concat))
+
+// add only the odd numbers in the array
+if (i==1){
+    return 'the number is odd'
+}
+
+// count how many numbers are even in the array
+for (let i; i = 0; <  array.length - 1 ; i++){
+    return 'even numbers'
+}
+// add the numbers that are NOT divisible by 3
+function notDivisible(){
+    for (let i; i = 0; < NOt .length - 1 ; i++){
+        return
+    }
+}
+  
