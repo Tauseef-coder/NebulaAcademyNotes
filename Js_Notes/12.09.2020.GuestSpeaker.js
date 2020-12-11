e28ac3e0d6156bbf1be117bedcc8031689c7b941
Tauseef-Coder <lm4cg33k@aol.com>
2020-12-11 13:51:19 -0500

    homework

---
+++ Js_Notes/12.09.2020.GuestSpeaker.js
@@ -0,0 +1,4 @@
+/*hi , i wanted to reach out and connect with you after listening to your presentation to nebula academy . 
+the  life experience you presented has had a positive impact on my desire to pursue a career along with , boosting my desire to  really connected with other in the field 
+sincerely ,
+Tauseef fordon*/
---
+++ Js_Notes/12.09.2020.Home_workcodewars.js
@@ -0,0 +1,50 @@
+//Quarter of the year
+
+https://www.codewars.com/kata/5ce9c1000bab0b001134f5af
+
+const quarterOf = (month) => {
+    // Your code here
+    if (month >= 1 && month <= 3) {
+      return 1;
+    } else if (month >= 4 && month <= 6) {
+      return 2;
+    } else if (month >= 7 && month <= 9) {
+      return 3;
+    } else {
+      return 4;
+    }
+  }//You have passed all of the tests! :)
+  
+ 
+
+//Multiplication Table
+
+https://www.codewars.com/kata/5a2fd38b55519ed98f0000ce
+
+ 
+
+//Enough Space
+
+https://www.codewars.com/kata/5875b200d520904a04000003
+
+
+
+
+
+
+//Reversed Sequence
+
+https://www.codewars.com/kata/5a00e05cc374cb34d100000d
+
+//Switch it up
+
+
+
+https://www.codewars.com/kata/5808dcb8f0ed42ae34000031
+
+
+
+
+//A Wolf in Sheep's Clothing
+
+https://www.codewars.com/kata/5c8bfa44b9d1192e1ebd3d15
